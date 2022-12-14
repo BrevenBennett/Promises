2bb5599c6dd2ac82c72cb4c0a5a45956c0df46f5
BrevenBennett <Breven.stars@gmail.com>
2022-12-14 10:33:26 -0600

    learned the fundamentals of promises

--- app.js
+++ app.js
@@ -1,14 +1,82 @@
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-const emailRef = document.querySelector(".email");
+// ~~~Example 1~~~
+// const emailRef = document.querySelector(".email");
 
 // 1. Then
-fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    emailRef.innerHTML = data.email;
-  });
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email;
+//   });
 
 // 2. Async/Await
+// async function main() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+//   const data = await response.json()
+//   console.log(data)
+//   emailRef.innerHTML = data.email
+// }
+
+// main();
+
+// ~~~Example 2~~~
+// const statusRef = document.querySelector('.status')
+
+// function getSubscriptionStatus() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("VIP");
+//     }, 2000);
+//   });
+// }
+
+// 1. Then
+// getSubscriptionStatus().then(response => console.log(response))
+
+// 2. Async/Await
+// async function main() {
+//   const status = (await getSubscriptionStatus());
+//   statusRef.innerHTML = status;
+// }
+
+// main();
+
+// ~~~Problem~~~
+const statusRef = document.querySelector(".status");
+const videoRef = document.querySelector(".video")
+
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(undefined);
+    }, 2000);
+  });
+}
+
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
+  });
+}
+
+async function main() {
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  try {
+      console.log(await getVideo(status));
+  }
+  catch (e) {
+    console.log(e)
+    videoRef.innerHTML = e
+  }
+}
+
+main();
